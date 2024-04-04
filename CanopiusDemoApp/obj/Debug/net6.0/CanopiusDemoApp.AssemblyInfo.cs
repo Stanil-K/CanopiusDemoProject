201ab86be6169df802e9b689faf70318d771a742
Kalaydzhiev, Stanil <Stanil.Kalaydzhiev@canopius.com>
2024-04-04 12:52:46 +0100

    change

--- CanopiusDemoApp/obj/Debug/net6.0/CanopiusDemoApp.AssemblyInfo.cs
+++ CanopiusDemoApp/obj/Debug/net6.0/CanopiusDemoApp.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CanopiusDemoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eaa67bae790d77e58606608b33db43c4cbcadd53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c27a1a064bc009a1f243e07ebf839b89f243a4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CanopiusDemoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CanopiusDemoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
